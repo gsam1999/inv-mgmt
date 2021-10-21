2702004c5241475de4425ebeac5b52e56636cc01
sumanth <gsumanth@protonmail.com>
2021-10-22 00:56:30 +0530

    new uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURI: 'https://ec2-65-1-130-86.ap-south-1.compute.amazonaws.com:3443/'
+  apiURI: 'http://ec2-65-1-130-86.ap-south-1.compute.amazonaws.com:3000/'
 };
